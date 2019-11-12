83499413dce4bd7cdaad96eebf18eb8c547b3a05
sravanthiBairaboina <57647916+sravanthiBairaboina@users.noreply.github.com>
2019-11-11 22:25:43 -0600

    Add files via upload

---
+++ RewardSummary.java
@@ -0,0 +1,48 @@
+package com.client.rewards.summary;
+
+import java.util.*;
+
+public class RewardSummary {
+	public static void main(String[] args) {
+
+		// Lists of invoices for each month
+		List<Long> invoice_jan_customer1 = Arrays.asList(Long.valueOf(120), Long.valueOf(60));
+		List<Long> invoice_feb_customer1 = Arrays.asList(Long.valueOf(40), Long.valueOf(120), Long.valueOf(30));
+		List<Long> invoice_mar_customer1 = Arrays.asList(Long.valueOf(150));
+
+		// Map with list of invoices for each month
+		Map<String, List<Long>> map_customer1 = new HashMap<>();
+		map_customer1.put("jan", invoice_jan_customer1);
+		map_customer1.put("feb", invoice_feb_customer1);
+		map_customer1.put("mar", invoice_mar_customer1);
+
+		long totalRewardPointsForThreeMonths = calculateRewardPointsForThreeMonths(map_customer1);
+		System.out.println("totalRewardPointsForThreeMonths :: " + totalRewardPointsForThreeMonths);
+
+	}
+
+	// Three months reward point addition for each Customer
+	private static long calculateRewardPointsForThreeMonths(Map<String, List<Long>> invoicesOfThreeMonths) {
+		return invoicesOfThreeMonths.entrySet().stream().mapToLong((e) -> {
+			return rewardPointsforEachMonth(e.getValue());
+		}).sum();
+	}
+
+	// Total reward point addition of each month's invoice
+	private static long rewardPointsforEachMonth(final List<Long> invoicesInEachmonth) {
+		return invoicesInEachmonth.stream().mapToLong((transactionAmt) -> {
+			return rewardPointsForEachInvoice(transactionAmt);
+		}).sum();
+	}
+
+	// reward points for each invoice
+	private static long rewardPointsForEachInvoice(final long invoiceAmount) {
+		if (invoiceAmount >= 50 && invoiceAmount <= 100) {
+			return invoiceAmount - 50;
+		} else if (invoiceAmount > 100) {
+			return (invoiceAmount - 100) * 2 + 50;
+		} else {
+			return 0;
+		}
+	}
+}
